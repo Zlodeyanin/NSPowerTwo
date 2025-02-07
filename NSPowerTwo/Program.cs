05ab4a00817d5a7a15014d5871a55713b1cd10c7
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2025-02-07 09:01:17 +0300

    Добавьте файлы проекта.

---
+++ NSPowerTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NSPowerTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int startNumber = 2;
+            int extent = 2;
+            int extentCount = 0;
+            int number = startNumber;
+            int minRandomValue = 0;
+            int maxRandomValue = 100;
+            int randomNumber = random.Next(minRandomValue, maxRandomValue);
+            
+            while (number < randomNumber)
+            {
+                number *= extent;
+                extentCount++;
+            }
+            Console.WriteLine($"Случайное число: {randomNumber}.\nСтепень двойки: {extentCount}.\nПревосходящее число: {number}.");
+        }
+    }
+}
